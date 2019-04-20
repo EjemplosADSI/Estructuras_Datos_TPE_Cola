3c66fcfdd29416c1ff0232d28f4bc1d311ead192
Diego Alonso Ojeda Medina <daom89@gmail.com>
2019-04-20 18:12:03 -0500

    Codigo Fuente: Estructura de datos en C# - Cola - Parte 2 - Buscar Nodo => http://bit.ly/2GmXRcY

--- Cola.cs
+++ Cola.cs
@@ -24,6 +24,37 @@ namespace Estructuras_Datos_TPE_Cola
 
         }
 
+        public void buscarNodo()
+        {
+            Nodo Actual = new Nodo();
+            Actual = Primero;
+            bool Encontrado = false;
+            System.Console.WriteLine(" Ingrese el dato del nodo a Buscar: ");
+            int nodoBuscado = int.Parse(Console.ReadLine());
+
+            if (Primero != null)
+            {
+                while (Actual != null && Encontrado != true)
+                {
+                    if(Actual.Dato == nodoBuscado)
+                    {
+                        System.Console.WriteLine("\n El nodo con el dato ( {0} ) Encontrado", nodoBuscado);
+                        Encontrado = true;
+                    }
+                    Actual = Actual.Siguiente;
+                }
+
+                if (!Encontrado)
+                {
+                    System.Console.WriteLine("\n Nodo no encontrado\n");
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("\n La cola se encuentra vacia\n\n");
+            }
+        }
         public void desplegarCola()
         {
             Nodo Actual = new Nodo();
--- Program.cs
+++ Program.cs
@@ -8,14 +8,49 @@ namespace Estructuras_Datos_TPE_Cola
         {
             Cola c = new Cola();
 
-            c.insertarNodo();
-            c.insertarNodo();
-            c.insertarNodo();
-            c.insertarNodo();
-            c.insertarNodo();
+            int opcionMenu = 0;
 
-            System.Console.WriteLine("\n Cola Ingresada:\n");
-            c.desplegarCola();
+            do
+            {
+             System.Console.WriteLine("\n|-------------------------------|");
+                System.Console.WriteLine("|     * Concepto de Cola *      |");
+                System.Console.WriteLine("|---------------|---------------|");
+                System.Console.WriteLine("| 1. Insertar   | 4. Eliminar   |");
+                System.Console.WriteLine("| 2. Buscar     | 5. Desplegar  |");
+                System.Console.WriteLine("| 3. Modificar  | 6. Salir      |");
+                System.Console.WriteLine("|---------------|---------------|");
+                System.Console.WriteLine("|     Selecciona una Opcion     |");
+                opcionMenu = int.Parse(Console.ReadLine());
+                switch (opcionMenu)
+                {
+                    case 1:
+                        System.Console.WriteLine("\n\n Insertar un nodo en la cola \n");
+                        c.insertarNodo();
+                        break;
+                    case 2:
+                        System.Console.WriteLine("\n\n Buscar un nodo en la cola \n");
+                        c.buscarNodo();
+                        break;
+                    case 3:
+                        System.Console.WriteLine("\n\n Modificar un nodo en la cola \n");
+                        //c.modificarNodo();
+                        break;
+                    case 4:
+                        System.Console.WriteLine("\n\n Eliminar un nodo en la cola \n");
+                        //c.eliminarNodo();
+                        break;
+                    case 5:
+                        System.Console.WriteLine("\n\n Desplegar cola de nodos \n");
+                        c.desplegarCola();
+                        break;
+                    case 6:
+                        System.Console.WriteLine("\n\n Programa Finalizado.... \n");
+                        break;
+                    default:
+                        System.Console.WriteLine("\n\n Opcion No Valida.... \n");
+                        break;
+                }
+            } while (opcionMenu != 6);
         }
     }
 }
