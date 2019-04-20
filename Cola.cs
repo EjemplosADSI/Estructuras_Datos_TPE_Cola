9c8620b3172c2faef20b58b1efe773d08c287a6a
Diego Alonso Ojeda Medina <daom89@gmail.com>
2019-04-20 18:52:07 -0500

    Correccion de Error en Metodo Insertar

--- Cola.cs
+++ Cola.cs
@@ -5,25 +5,37 @@ namespace Estructuras_Datos_TPE_Cola
     public class Cola
     {
         private Nodo Primero = new Nodo();
+        private Nodo Ultimo = new Nodo();
 
         public Cola()
         {
             Primero = null;
+            Ultimo = null;
         }
 
         public void insertarNodo()
         {
-            Nodo nuevo = new Nodo();
-            System.Console.WriteLine("Ingrese el dato de la pila");
-            nuevo.Dato = int.Parse(Console.ReadLine());
+            Nodo Nuevo = new Nodo();
+            System.Console.WriteLine("Ingrese el dato del nuevo nodo: ");
+            Nuevo.Dato = int.Parse(Console.ReadLine());
             
-            nuevo.Siguiente = Primero;
-            Primero = nuevo;
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+                Primero.Siguiente = null;
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Nuevo.Siguiente = null;
+                Ultimo = Nuevo;
+                
+            }
             
             System.Console.WriteLine("\n Nodo Ingresado\n\n");
 
         }
-
         public void buscarNodo()
         {
             Nodo Actual = new Nodo();
@@ -89,6 +101,56 @@ namespace Estructuras_Datos_TPE_Cola
             else
             {
                 System.Console.WriteLine("\n La cola se encuentra vacia\n\n");
+            }
+        }
+
+        public void eliminarNodo()
+        {
+            Nodo Actual = new Nodo();
+            Actual = Primero;
+            Nodo Anterior = new Nodo();
+            Anterior = null; 
+
+            bool Encontrado = false;
+            System.Console.WriteLine(" Ingrese el dato del nodo a buscar para eliminarlo");
+            int nodoBuscado = int.Parse(Console.ReadLine());
+
+            if (Primero != null)
+            {
+                while (Actual != null && Encontrado != true)
+                {
+                    if(Actual.Dato == nodoBuscado)
+                    {
+                        System.Console.WriteLine("\n El nodo con el dato ( {0} ) Encontrado\n", nodoBuscado);
+                        
+                        if (Actual == Primero)
+                        {
+                            Primero = Primero.Siguiente;
+                        }else if ( Actual == Ultimo)
+                        {
+
+                        }
+                        else
+                        {
+                            Anterior.Siguiente = Actual.Siguiente;
+                        }
+                        System.Console.WriteLine("\n El nodo Eliminado\n");
+                        
+                        Encontrado = true;
+                    }
+                    Anterior = Actual;
+                    Actual = Actual.Siguiente;
+                }
+
+                if (!Encontrado)
+                {
+                    System.Console.WriteLine("\n Nodo no encontrado\n");
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("\n La pila se encuentra vacia\n\n");
             }
         }
 
